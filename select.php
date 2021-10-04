41c3b53dd1ad5fc9b5dce19ffa6cd3a1b5a4249e
entradaalreino <91288145+entradaalreino@users.noreply.github.com>
2021-10-04 22:04:00 +0200

    php tests

---
+++ select.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+$host = "sql2.freesqldatabase.com";
+$user = "sql2318442";
+$pass = "xM1%xK7!";
+$db = "historial";
+
+$con = mysql_connect($host, $user, $pass);
+mysql_select_db($db, $con);
+
+/*
+include('connection.php');
+
+$sql = "SELECT iglesia,cura FROM historial WHERE cura=0 ORDER BY fecha DESC LIMIT 1";
+$result = mysqli_query($connect, $sql);
+
+if (mysqli_num_rows($result)>0){
+    while($row = mysqli_fetch_assoc($result)){
+        echo "iglesia:".$row['iglesia']."|email:".$row['cura'].";";
+    }
+}
+*/
+?>
